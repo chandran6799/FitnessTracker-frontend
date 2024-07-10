4b789c6b4df5ed13663ed4e1bf974760bfbb7472
chandran6799 <chandranak06071999@gmail.comgit config --global user.email chandranak06071999@gmail.com>
2024-07-10 01:05:43 -0700

    fitness commit

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -2,8 +2,8 @@ import React from "react";
 
 const Home = () => {
   return (
-    <div className="md:flex md:justify-content-between mb-3">
-      <div className="md:-ml-32">
+    <div className="md:flex md:justify-content-between mb-5 mt-3 md:-ml-14">
+      <div className="md:-ml-32 mt-5">
         <h5 className="text-3xl font-semibold md:font-semibold md:text-5xl md:mt-28 md:ml-96">
           Exercise is the key to{" "}
           <span className="text-fuchsia-600 underline decoration-from-font underline-offset-4">
@@ -11,13 +11,13 @@ const Home = () => {
           </span>{" "}
           Lifestyle
         </h5>
-        <p className="md:text-sm md:ml-96 md:mt-3">
+        <p className="md:text-sm md:ml-96 md:mt-3 mr-20">
           "Exercise not only changes your body, it changes your mind, your
           attitude and your mood"
         </p>
       </div>
       <div>
-        <img src="fitness.png" />
+        <img src="https://img.freepik.com/free-vector/man-sport-activities_102902-2338.jpg" />
       </div>
     </div>
   );
