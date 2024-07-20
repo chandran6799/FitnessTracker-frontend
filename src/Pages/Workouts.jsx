9fde4104805ef8ccdfd4691c29f67cd07c63bbb9
chandran6799 <chandranak06071999@gmail.comgit config --global user.email chandranak06071999@gmail.com>
2024-07-19 20:49:10 -0700

    workout Commit

--- src/Pages/Workouts.jsx
+++ src/Pages/Workouts.jsx
@@ -2,59 +2,65 @@ import React from 'react';
 
 const Workouts = () => {
     return (
-        <div className='p-5 flex flex-wrap justify-between'>   
-            <div className="mb-5 max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
-        <img className="rounded-t-lg mt-3" src="https://miro.medium.com/v2/resize:fit:602/1*DWkCsrWkx8eBSTq-xmZCSw.jpeg" alt="workout" />
-    
-    <div className="p-5">
-        
-            <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Shoulder Workout</h5>
-        <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">Shoulder strength training can reduce your risk of injury by strengthening your core muscles, which makes you more stable and lessens imbalances.</p>
-    </div>
-</div>
-<div className="mb-5 max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
-        <img className="ml-16 -mt-11" src="https://apilyfta.com/static/GymvisualPNG/11651101-Barbell-Standing-Military-Press-(without-rack)_Shoulders-FIX_small.png" alt="workout" />
-    
-    <div className="p-5">
-        
-            <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Military Press</h5>
-        <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">Strengthens your shoulders, traps, and triceps. Improves core strength, balance, and stability. Builds upper body power and strength.</p>
-    </div>
-</div>
-<div className="mb-5 max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+        <div className='p-5 flex flex-wrap justify-between'> 
+        {/* 1   */}
+            <div className="mb-5 max-w-sm bg-white border border-gray-200 rounded-lg shadow">
         <img className="rounded-t-lg" src="https://www.dmoose.com/cdn/shop/articles/1_8db57df7-b8f2-412e-a364-745224090211.jpg?v=1647872727" alt="workout" />
     
-    <div className="p-5 -mt-5">
+    <div className="p-5">
         
-            <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">dumbbell upright row</h5>
-        <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">Plank rows can also be varied to target different muscle groups, making them an ideal exercise for a full-body workout.</p>
+            <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900">Shoulder: Dumbbell Upright Row</h5>
+        <p className="mb-3 font-normal text-zinc-600">Plank rows can also be varied to target different muscle groups, making them an ideal exercise for a full-body workout.</p>
     </div>
 </div>
-<div className="mb-5 max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
-        <img className="rounded-t-lg w-44 ml-20 mt-3" src="https://www.inspireusafoundation.org/wp-content/uploads/2022/12/dumbbell-push-press-second-position-701x1024.png" alt="workout" />
+ {/* 2   */}
+<div className="mb-5 max-w-sm bg-white border border-gray-200 rounded-lg shadow">
+        <img className="rounded-t-lg" src="https://cdn.shopify.com/s/files/1/1497/9682/files/2_a09de347-1652-4b84-96bf-fdc8bbc42481.jpg?v=1648825457" alt="workout" />
     
     <div className="p-5">
         
-            <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Push Press</h5>
-        <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">Push presses are an excellent barbell exercise for athletes of all kinds. They help build upper body explosiveness, improve shoulder health, increase core strength and stability.</p>
+            <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900">Chest: Barbell Bench Press</h5>
+        <p className="mb-3 font-normal text-zinc-600">Bench presses can be used to increase strength, improve endurance, and tone the muscles of the upper body, including the chest, arms, and shoulders.</p>
     </div>
 </div>
-<div className="mb-5 max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+ {/* 3  */}
+<div className="mb-5 max-w-sm bg-white border border-gray-200 rounded-lg shadow">
+        <img className="rounded-t-lg bg-zinc-100 h-64" src="https://images.squarespace-cdn.com/content/v1/5ffcea9416aee143500ea103/1638429761902-YGOBY5N9OELVS044EGND/Barbell%2BFront%2BSquats.png" alt="workout" />
+    
+    <div className="p-5">
+        
+            <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900">Leg: Front Barbell Squat</h5>
+        <p className="mb-3 font-normal text-zinc-600">The front squat muscles worked primarily include the quadriceps (quads), glutes, hips, and hamstrings. If your goal is to build the perfect glutes, front squats can help with that too. They also help strengthen the spinal erectors.</p>
+    </div>
+</div>
+ {/* 4   */}
+<div className="mb-5 max-w-sm bg-white border border-gray-200 rounded-lg shadow">
+        <img className="rounded-t-lg mt-2 ml-16" src="https://images.squarespace-cdn.com/content/v1/55e406fbe4b0b03c5e7543ae/1493153222193-615KSYPSV1K197RWRHWO/Standing+EZ+Bar+Curls.jpg" alt="workout" />
+    
+    <div className="p-5">
+        
+            <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900">Arms: Concentration Curls</h5>
+        <p className="mb-3 font-normal text-zinc-600">Concentration curls are a great way to build strength in the biceps and forearm muscles. They can be performed with dumbbells, barbells, or cables. They can also be done with one arm at a time.</p>
+    </div>
+</div>
+ {/* 5   */}
+<div className="mb-5 max-w-sm bg-white border border-gray-200 rounded-lg shadow">
         <img className="rounded-t-lg mt-3" src="https://qph.cf2.quoracdn.net/main-qimg-5cc29b14455feb115e6e463dac1b630b-lq" alt="workout" />
     
-    <div className="p-5 mt-16">
+    <div className="p-5 mt-12">
         
-            <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Reverse Fly</h5>
-        <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">The reverse fly is an exercise you can do with dumbbells to target the muscles in the upper back. Strong upper back muscles help balance your shoulder strength and help protect your shoulder from injury.</p>
+            <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900">Shoulder: Reverse Fly</h5>
+        <p className="mb-3 font-normal text-zinc-600">The reverse fly is an exercise you can do with dumbbells to target the muscles in the upper back. Strong upper back muscles help balance your shoulder strength.</p>
     </div>
 </div>
-<div className="mb-5 max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
-        <img className="rounded-t-lg" src="https://cdn.shopify.com/s/files/1/1497/9682/files/1.How_to_Do_The_Arnold_Press.jpg?v=1673014027" alt="workout" />
+ {/* 6   */}
+<div className="mb-5 max-w-sm bg-white border border-gray-200 rounded-lg shadow">
+        <img className="rounded-t-lg mt-3" src="https://kinxlearning.com/cdn/shop/files/exercise-55_1000x.jpg?v=1613157966" alt="workout" />
     
-    <div className="p-5 mt-2">
+    <div className="p-5">
         
-            <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Arnold Press</h5>
-        <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">Stronger bones, it can increase your bone density and reduce the risk of osteoporosis, which weakens your bones and makes them brittle.</p>
+            <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900">Back Wings: Overhead Press</h5>
+        <p className="mb-3 font-normal text-zinc-600">It can help you increase muscle mass, improve shoulder strength and stability, increase your range of motion, and improve your posture.</p>
     </div>
 </div>
         </div>
